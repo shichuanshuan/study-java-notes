cc4a0f605daf2a7db30ca9274e5dddead40afe30
shichuanshuan <shidashuan@qq.com>
2024-01-11 17:16:22 +0800

    add string type

--- P54_强制类型转换/ForceConvertDetail.java
+++ P54_强制类型转换/ForceConvertDetail.java
@@ -1,3 +1,14 @@
 public class ForceConvertDetail {
-	
+	public static void main(String[] args) {
+		// 强制符号只针对最近的操作时有效，往往会使用小括号提升优先级
+		// int x = (int)30 * 3.5 + 6 * 1.5; // 编译错误：double -> int
+		int x = (int)(30 * 3.5 + 6 * 1.5);
+		System.out.println(x);
+
+		char c1 = 100;
+		int m = 100;
+		// char c3 = m; // 错误
+		char c3 = (char)m;
+		System.out.println(c3);
+	}
 }
