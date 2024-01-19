1a421a37b4ca868530494c751832e7589a332608
shichuanshuan <shidashuan@qq.com>
2024-01-19 16:22:13 +0800

    add Bit operator

---
+++ P100_位运算详解/BitOperator2.java
@@ -0,0 +1,11 @@
+public class BitOperator2 {
+	public static void main(String[] args) {
+		System.out.println(1>>2); // Ïàµ±ÓÚ 1 / 2 / 2 = 0
+		System.out.println(1 << 2); // 1 * 2 * 2 = 4
+		System.out.println(4 << 3); // 4 * 2 * 2 * 2 = 32
+		System.out.println(15 >> 2); // 15 / 2 / 2 = 3
+
+
+		System.out.println(-1 >> 2);
+	}
+}
